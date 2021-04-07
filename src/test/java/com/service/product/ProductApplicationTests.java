b2c684ecbe59b7fd6bf267ad71b7f4dce268bcce
harika298 <harika.lokavarapu223@gmail.com>
2021-04-07 20:34:01 +0530

    eureka integration

--- src/test/java/com/service/product/ProductApplicationTests.java
+++ src/test/java/com/service/product/ProductApplicationTests.java
@@ -1,13 +1,13 @@
-package com.service.product;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ProductApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.service.product;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ProductApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
